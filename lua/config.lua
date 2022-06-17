c652e83e5bab262cd31b1e5cb9c49b3c3832712f
Elijah Danko <me@elijahdanko.net>
2022-06-17 10:41:33 +0200

    Handle <Down> and <Up> on complete menu

--- lua/config.lua
+++ lua/config.lua
@@ -246,7 +246,9 @@ function config.cmp()
     },
     mapping = {
       ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<Up>"] = cmp.mapping.select_prev_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<Down>"] = cmp.mapping.select_next_item(),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-u>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),

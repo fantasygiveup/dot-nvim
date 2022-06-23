d5094e567823334b4bc65463bf3cf3cf65508300
Elijah Danko <me@elijahdanko.net>
2022-06-23 16:01:51 +0200

    Use buffer / win only settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.o.conceallevel = 1 -- turn on conceal feature
+vim.wo.conceallevel = 1 -- turn on conceal feature
 vim.keymap.set("n", ",ww", "<Cmd>MarkdownPreviewToggle<CR>")
--- after/ftplugin/org.lua
+++ after/ftplugin/org.lua
@@ -1,3 +1,2 @@
-vim.o.conceallevel = 1 -- turn on conceal feature
-vim.o.linebreak = 0
-vim.o.formatoptions = vim.o.formatoptions:gsub('c', '')  -- turn off autowrap links
+vim.wo.conceallevel = 1 -- turn on conceal feature
+vim.bo.formatoptions = vim.o.formatoptions:gsub('c', '')  -- turn off autowrap links

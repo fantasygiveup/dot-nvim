c28acc69ca109c1cdd0e5517f16a63b9ba62e940
Elijah Danko <me@elijahdanko.net>
2022-10-02 16:16:34 +0200

    Lualine: update lsp sign

--- lua/config.lua
+++ lua/config.lua
@@ -86,7 +86,7 @@ M.status_line = function()
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return "⚙️ " .. client.name
+        return " " .. client.name
       end
     end
     return ""

6692450e6d74eb1f66e2aed44bea4cdf11ab98fa
Elijah Danko <me@elijahdanko.net>
2022-10-06 22:46:42 +0200

    Snip: add csmain cpp

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -116,3 +116,31 @@ end
     )
   ),
 })
+
+ls.add_snippets("cpp", {
+  s(
+    "csmain",
+    fmt(
+      [[
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+int main() {{
+  {}
+  return 0;
+}}
+  ]],
+      { i(0) }
+    )
+  ),
+})

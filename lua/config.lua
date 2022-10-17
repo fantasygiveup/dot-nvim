fc0ce9e4c79bcf8434310f516e8c24b769171429
Elijah Danko <me@elijahdanko.net>
2022-10-17 20:30:09 +0200

    Fix depricated

--- lua/config.lua
+++ lua/config.lua
@@ -260,7 +260,7 @@ M.lsp = function()
 
   local nvim_lsp = require("lspconfig")
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
   local servers = { "ccls", "gopls", "pyright", "tsserver" }
   for _, lsp in ipairs(servers) do

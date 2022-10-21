8444ccd6423af6ea0081cd53300cb2ff52fac993
Illia Danko <me@idanko.org>
2022-10-21 16:43:08 +0200

    Update name

--- lua/global.lua
+++ lua/global.lua
@@ -3,27 +3,20 @@ local global = {}
 local home = os.getenv("HOME")
 local path_sep = global.is_windows and "\\" or "/"
 local os_name = vim.loop.os_uname().sysname
-local user_repo = home .. path_sep .. "github.com" .. path_sep .. "elijahdanko" .. path_sep
+local user_repo = home .. path_sep .. "github.com" .. path_sep .. "illia-danko" .. path_sep
 
 function global:load_variables()
   self.is_mac = os_name == "Darwin"
   self.is_linux = os_name == "Linux"
   self.is_windows = os_name == "Windows"
   self.vim_path = vim.fn.stdpath("config")
-  self.notes_dir = home
-    .. path_sep
-    .. "github.com"
-    .. path_sep
-    .. "elijahdanko"
-    .. path_sep
-    .. "docs"
+  self.notes_dir = user_repo .. "docs"
   self.cache_dir = home .. path_sep .. ".cache" .. path_sep .. "nvim" .. path_sep
   self.undo_dir = home .. path_sep .. ".cache" .. path_sep .. "undo" .. path_sep
   self.modules_dir = self.vim_path .. path_sep .. "modules"
   self.path_sep = path_sep
   self.home = home
-  self.user_name = "Elijah Danko"
-  self.email = "me@elijahdanko.net"
+  self.user_name = "Illia Danko"
 end
 
 function global:ensure_dir()

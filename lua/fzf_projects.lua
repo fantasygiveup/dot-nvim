6267eb84e899997cd008cf39436a1ca8db9b1a6e
Illia Danko <me@elijahdanko.net>
2022-11-06 12:14:50 +0100

    Add fzf_projects tool #2

--- lua/fzf_projects.lua
+++ lua/fzf_projects.lua
@@ -1,6 +1,7 @@
--- Navigate and open projects on the system using fzf and fd commands.
+-- Navigate and open projects on the system using 'fzf', 'fd', 'awk' and 'tree' commands.
+-- It is better integrated with auto path detection tool such as 'ahmedkhalf/project.nvim'.
 
-local ok, fzf = pcall(require, "fzf-lua")
+local ok, fzf_lua = pcall(require, "fzf-lua")
 if not ok then
   vim.notify("Could not find fzf-lua module", vim.log.levels.ERROR)
   return
@@ -13,13 +14,18 @@ local root_path = os.getenv("HOME")
 local unique_cmd = "awk '!x[$0]++'"
 local preview_cmd = "tree -C -L 1 {}"
 local prompt = "Projects> "
-local fd_cmd = "fd --hidden --case-sensitive --absolute-path --exec echo '{//}' ';' "
+local fd_exec = "fd"
+if vim.fn.executable("fdfind") == 1 then
+  fd_exec = "fdfind"
+end
+local fd_cmd = fd_exec
+  .. " --hidden --case-sensitive --absolute-path --exec echo '{//}' ';' "
   .. pattern
   .. " "
   .. root_path
 
 M.navigate = function()
-  fzf.fzf_exec(fd_cmd .. " | " .. unique_cmd, {
+  fzf_lua.fzf_exec(fd_cmd .. " | " .. unique_cmd, {
     preview = preview_cmd,
     prompt = prompt,
     actions = {
@@ -27,7 +33,7 @@ M.navigate = function()
         if #selected < 1 then
           return
         end
-        fzf.files({ cwd = selected[1] })
+        fzf_lua.files({ cwd = selected[1] })
       end,
     },
   })

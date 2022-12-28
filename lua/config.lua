a12a9411c7f65ed9b0599fbd02b27d8f9fd802e0
illiadanko <illia.a.danko@gmail.com>
2022-12-28 23:01:35 +0100

    Fix completion keymap, behaviour

--- lua/config.lua
+++ lua/config.lua
@@ -243,6 +243,13 @@ M.luasnip = function()
 end
 
 M.cmp = function()
+  local function has_words_before()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0
+      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
   local cmp = require("cmp")
   local luasnip = require("luasnip")
   local lspkind = require("lspkind")
@@ -261,29 +268,30 @@ M.cmp = function()
       ["<Up>"] = cmp.mapping.select_prev_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
       ["<Down>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-d>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      ["<Tab>"] = function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
-      end,
-      ["<S-Tab>"] = function(fallback)
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function()
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
         end
-      end,
+      end, { "i", "s" }),
     },
     sources = {
       { name = "luasnip", priority = 9 },

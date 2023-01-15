017c297fe0d1cea112ab4de1a747fa71a3f65730
illiadanko <illia.a.danko@gmail.com>
2023-01-15 08:27:11 +0100

    Notes: better keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -105,9 +105,9 @@ M.plugins = function()
   vim.keymap.set("n", "<localleader>z", "<cmd>lua require'internal'.zen_mode()<cr>")
 
   vim.keymap.set("n", "<leader>od", "<cmd>lua require'notes'.diary_new_entry()<cr>")
-  vim.keymap.set("n", "<leader>oo", "<cmd>lua require'notes'.diary_open_file()<cr>")
+  vim.keymap.set("n", "<leader>oy", "<cmd>lua require'notes'.diary_open_file()<cr>")
   vim.keymap.set("n", "<leader>ot", "<cmd>lua require'notes'.todos_new_entry()<cr>")
-  vim.keymap.set("n", "<leader>ol", "<cmd>lua require'notes'.todos_open_file()<cr>")
+  vim.keymap.set("n", "<leader>os", "<cmd>lua require'notes'.todos_open_file()<cr>")
 
   -- Debugger.
   vim.keymap.set("n", "<localleader>d<Space>", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")

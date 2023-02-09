a8796209c5e44d31f99d9fa454f20145758b61aa
illiadanko <illia.a.danko@gmail.com>
2023-02-09 17:48:40 +0100

    Update edit_completion.lua

--- lua/edit_completion.lua
+++ lua/edit_completion.lua
@@ -2,17 +2,17 @@ local M = {}
 
 M.setup = function(use)
   use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      { "L3MON4D3/LuaSnip" },
-      { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "saadparwaiz1/cmp_luasnip" },
-      { "andersevenrud/cmp-tmux" },
-      { "hrsh7th/cmp-calc" },
-    },
-    config = M.cmp,
+      "hrsh7th/nvim-cmp",
+      requires = {
+          { "L3MON4D3/LuaSnip" },
+          { "hrsh7th/cmp-path" },
+          { "hrsh7th/cmp-buffer" },
+          { "hrsh7th/cmp-nvim-lsp" },
+          { "saadparwaiz1/cmp_luasnip" },
+          { "andersevenrud/cmp-tmux" },
+          { "hrsh7th/cmp-calc" },
+      },
+      config = M.cmp,
   })
   use({ "neovim/nvim-lspconfig", config = M.lspconfig })
 end
@@ -36,56 +36,56 @@ M.cmp = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0
-      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
   cmp.setup({
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select", count = 1 }),
-      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select", count = 1 }),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+      },
+      window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select", count = 1 }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select", count = 1 }),
+          ["<C-b>"] = cmp.mapping.scroll_docs( -4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
 
-      ["<S-Tab>"] = cmp.mapping(function()
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        end
-      end, { "i", "s" }),
-    }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-    }, {
-      { name = "buffer" },
-      { name = "tmux", option = { all_panes = true, label = "" } },
-      { name = "path" },
-      { name = "calc" },
-    }),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+      }, {
+          { name = "buffer" },
+          { name = "tmux",  option = { all_panes = true, label = "" } },
+          { name = "path" },
+          { name = "calc" },
+      }),
   })
 end
 
@@ -104,45 +104,45 @@ M.lspconfig = function()
       local group = vim.api.nvim_create_augroup("document_highlight_group", { clear = true })
 
       vim.api.nvim_create_autocmd({ "CursorHold" }, {
-        group = group,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.document_highlight()
-        end,
+          group = group,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.document_highlight()
+          end,
       })
 
       vim.api.nvim_create_autocmd({ "CursorMoved" }, {
-        group = group,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.clear_references()
-        end,
+          group = group,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.clear_references()
+          end,
       })
     end
   end
 
   local diagnostic_config = {
-    virtual_text = false,
-    signs = {
-      active = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
+      virtual_text = false,
+      signs = {
+          active = {
+              { name = "DiagnosticSignError", text = "" },
+              { name = "DiagnosticSignWarn",  text = "" },
+              { name = "DiagnosticSignHint",  text = "" },
+              { name = "DiagnosticSignInfo",  text = "" },
+          },
+          severity = vim.diagnostic.severity.ERROR,
       },
-      severity = vim.diagnostic.severity.ERROR,
-    },
-    update_in_insert = true,
-    underline = { severity = vim.diagnostic.severity.ERROR },
-    severity_sort = true,
-    float = {
-      focusable = false,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
+      update_in_insert = true,
+      underline = { severity = vim.diagnostic.severity.ERROR },
+      severity_sort = true,
+      float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+      },
   }
 
   for _, sign in ipairs(diagnostic_config.signs.active) do
@@ -157,18 +157,18 @@ M.lspconfig = function()
     local opts = { noremap = true, silent = true }
 
     vim.api.nvim_buf_set_keymap(
-      bufnr,
-      "n",
-      "]d",
-      "<cmd>lua vim.diagnostic.goto_next({ severity = require'global'.diagnostic_severity })<cr>",
-      opts
+        bufnr,
+        "n",
+        "]d",
+        "<cmd>lua vim.diagnostic.goto_next({ severity = require'global'.diagnostic_severity })<cr>",
+        opts
     )
     vim.api.nvim_buf_set_keymap(
-      bufnr,
-      "n",
-      "[d",
-      "<cmd>lua vim.diagnostic.goto_prev({ severity = require'global'.diagnostic_severity })<cr>",
-      opts
+        bufnr,
+        "n",
+        "[d",
+        "<cmd>lua vim.diagnostic.goto_prev({ severity = require'global'.diagnostic_severity })<cr>",
+        opts
     )
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
@@ -176,26 +176,26 @@ M.lspconfig = function()
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
     vim.api.nvim_buf_set_keymap(
-      bufnr,
-      "i",
-      "<C-h>",
-      "<cmd>lua vim.lsp.buf.signature_help()<cr>",
-      opts
+        bufnr,
+        "i",
+        "<C-h>",
+        "<cmd>lua vim.lsp.buf.signature_help()<cr>",
+        opts
     )
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gu", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
   end
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, diagnostic_config)
+      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, diagnostic_config)
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
+          border = "rounded",
+      })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+          border = "rounded",
+      })
   vim.diagnostic.config(diagnostic_config)
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -205,26 +205,26 @@ M.lspconfig = function()
   local servers = { "ccls", "gopls", "pyright", "tsserver", "rust_analyzer" }
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+        on_attach = on_attach,
+        capabilities = capabilities,
     })
   end
 
   -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua.
   lspconfig.sumneko_lua.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-      Lua = {
-        runtime = { version = "LuaJIT" },
-        diagnostics = { globals = { "vim" } },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-          checkThirdParty = false,
-        },
-        telemetry = { enable = false },
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+          Lua = {
+              runtime = { version = "LuaJIT" },
+              diagnostics = { globals = { "vim" } },
+              workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+              },
+              telemetry = { enable = false },
+          },
       },
-    },
   })
 end
 

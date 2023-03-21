b7a668c099effa1b4db00d27fb5339606451137f
illiadanko <illia.a.danko@gmail.com>
2023-03-21 20:25:40 +0100

    Switch to telescope

--- lua/fuzzy_finder.lua
+++ lua/fuzzy_finder.lua
@@ -1,70 +1,70 @@
 local M = {}
 
 M.config = function(use)
-  use({ "ibhagwan/fzf-lua", requires = { "kyazdani42/nvim-web-devicons" }, config = M.finder_setup })
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "kyazdani42/nvim-web-devicons" },
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+    },
+    config = M.finder_setup,
+  })
   use({ "stevearc/dressing.nvim", config = M.dressing_setup })
 end
 
 M.finder_setup = function()
-  local ok, fzf_lua = pcall(require, "fzf-lua")
+  local ok, telescope = pcall(require, "telescope")
   if not ok then
     return
   end
 
-  fzf_lua.setup({
-    winopts = {
-      fullscreen = true,
-      preview = {
-        vertical = "down:50%",
-        horizontal = "right:50%",
-        flip_columns = 160,
-        scrollbar = false,
+  telescope.setup({
+    extensions = { fzf = { fuzzy = false } },
+    defaults = {
+      prompt_prefix = "🔭 ",
+      -- Full screen, equal panels, prompt and content top.
+      layout_config = {
+        prompt_position = "top",
+        width = 1000,
+        height = 1000,
+        preview_width = 0.5,
+        anchor = "CENTER",
       },
-    },
-    keymap = {
-      builtin = {
-        ["<A-p>"] = "toggle-preview",
-        ["<C-f>"] = "preview-page-down",
-        ["<C-b>"] = "preview-page-up",
-      },
-      fzf = {},
+      sorting_strategy = "ascending",
+      vimgrep_arguments = require("vars").rg_opts,
     },
   })
 
+  telescope.load_extension("fzf")
+
   -- Fzf-lua keymap.
-  vim.keymap.set("n", "<localleader>b", "<cmd>lua require'fzf-lua'.buffers()<cr>")
+  vim.keymap.set(
+    "n",
+    "<localleader>b",
+    "<cmd>lua require'telescope.builtin'.buffers({sort_lastused = true})<cr>"
+  )
   vim.keymap.set(
     "n",
     "<c-t>",
-    "<cmd>lua require'fzf-lua'.files({ cmd = vim.env.FZF_DEFAULT_COMMAND })<cr>"
+    "<cmd>lua require'telescope.builtin'.find_files({find_command = require'vars'.fzf_default_command})<cr>"
   )
   vim.keymap.set("n", "<leader>:", "<cmd>lua require'fzf-lua'.commands()<cr>")
-  vim.keymap.set("n", "<leader>?", "<cmd>lua require'fzf-lua'.keymaps()<cr>")
-  vim.keymap.set("n", "<localleader>~", "<cmd>lua require'fzf-lua'.filetypes()<cr>")
-  vim.keymap.set("n", "<localleader>r", "<cmd>lua require'fzf-lua'.oldfiles()<cr>")
-  vim.keymap.set("n", "<localleader>gg", "<cmd>lua require'fzf-lua'.git_status()<cr>")
-  vim.keymap.set("n", "<localleader>gb", "<cmd>lua require'fzf-lua'.git_bcommits()<cr>")
-  vim.keymap.set("n", "<localleader>gl", "<cmd>lua require'fzf-lua'.git_commits()<cr>")
+
+  vim.keymap.set("n", "<leader>:", "<cmd>lua require'telescope.builtin'.commands()<cr>")
+  vim.keymap.set("n", "<leader>?", "<cmd>lua require'telescope.builtin'.keymaps()<cr>")
+  vim.keymap.set("n", "<localleader>~", "<cmd>lua require'telescope.builtin'.filetypes()<cr>")
+  vim.keymap.set("n", "<localleader>r", "<cmd>lua require'telescope.builtin'.oldfiles()<cr>")
+  vim.keymap.set("n", "<localleader>gg", "<cmd>lua require'telescope.builtin'.git_status()<cr>")
+  vim.keymap.set("n", "<localleader>gb", "<cmd>lua require'telescope.builtin'.git_bcommits()<cr>")
+  vim.keymap.set("n", "<localleader>gl", "<cmd>lua require'telescope.builtin'.git_commits()<cr>")
+  vim.keymap.set({ "n", "v" }, "<leader>/", "<cmd>lua require'telescope.builtin'.grep_string()<cr>")
   vim.keymap.set(
-    "v",
-    "<leader>/",
-    "<cmd>lua require'fzf-lua'.grep_visual({ rg_opts = require'vars'.rg_opts})<cr>"
+    "n",
+    "<c-s>",
+    "<cmd>lua require'telescope.builtin'.grep_string({ cwd = require'vars'.notes_dir })<cr>",
+    { desc = "grep_notes" }
   )
-
-  local function grep_project(opts)
-    local opts = opts or {}
-    opts.rg_opts = require("vars").rg_opts
-    opts.fzf_opts = { ["--nth"] = false }
-    opts.no_esc = true
-    opts.search = opts.search or "\\S"
-    fzf_lua.grep_project(opts)
-  end
-
-  vim.keymap.set("n", "<leader>/", grep_project, { desc = "grep_project" })
-
-  vim.keymap.set("n", "<c-s>", function()
-    grep_project({ prompt = "Notes> ", cwd = require("vars").notes_dir })
-  end, { desc = "grep_notes" })
 end
 
 M.dressing_setup = function()

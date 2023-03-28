70bcbc2a77138e9a441030c2cc36f713a9e33a58
illiadanko <illia.a.danko@gmail.com>
2023-03-28 10:21:01 +0200

    Jump to the char: jump to the current line

--- lua/edit.lua
+++ lua/edit.lua
@@ -71,8 +71,26 @@ M.hop_setup = function()
     { keys = "etovxqpdygfblzhckisuran" },
   })
 
+  local directions = hop_hint.HintDirection
   vim.keymap.set("", "f", function()
-    hop.hint_words()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+  end, { remap = true })
+  vim.keymap.set("", "F", function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+  end, { remap = true })
+  vim.keymap.set("", "t", function()
+    hop.hint_char1({
+      direction = directions.AFTER_CURSOR,
+      current_line_only = true,
+      hint_offset = -1,
+    })
+  end, { remap = true })
+  vim.keymap.set("", "T", function()
+    hop.hint_char1({
+      direction = directions.BEFORE_CURSOR,
+      current_line_only = true,
+      hint_offset = 1,
+    })
   end, { remap = true })
 end
 

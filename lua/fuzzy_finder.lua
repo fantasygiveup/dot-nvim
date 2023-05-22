eeb7bda6f5d0ca38af152c6f826f3b2a410453ca
illiadanko <illia.a.danko@gmail.com>
2023-05-23 00:32:51 +0200

    Remove commented out code

--- lua/fuzzy_finder.lua
+++ lua/fuzzy_finder.lua
@@ -75,38 +75,6 @@ M.config = function()
     "<cmd>lua require'telescope.builtin'.grep_string({ cwd = require'vars'.notes_dir, search = '' })<cr>",
     { desc = "grep_notes" }
   )
-
-  --
-  -- vim.keymap.set("n", "<localleader>b", "<cmd>lua require'fzf-lua'.buffers()<cr>")
-  -- vim.keymap.set(
-  --   "n",
-  --   "<c-t>",
-  --   "<cmd>lua require'fzf-lua'.files({ cmd = vim.env.FZF_DEFAULT_COMMAND })<cr>"
-  -- )
-  -- vim.keymap.set("n", "<leader>?", "<cmd>lua require'fzf-lua'.keymaps()<cr>")
-  -- vim.keymap.set("n", "<localleader>~", "<cmd>lua require'fzf-lua'.filetypes()<cr>")
-  -- vim.keymap.set("n", "<localleader>r", "<cmd>lua require'fzf-lua'.oldfiles()<cr>")
-  -- vim.keymap.set("n", "<localleader>gb", "<cmd>lua require'fzf-lua'.git_bcommits()<cr>")
-  -- vim.keymap.set("n", "<localleader>gl", "<cmd>lua require'fzf-lua'.git_commits()<cr>")
-  -- vim.keymap.set(
-  --   "v",
-  --   "<leader>/",
-  --   "<cmd>lua require'fzf-lua'.grep_visual({ rg_opts = require'vars'.rg_opts })<cr>"
-  -- )
-  --
-  -- local function grep_project(opts)
-  --   local opts = opts or {}
-  --   opts.rg_opts = require("vars").rg_opts
-  --   opts.fzf_opts = { ["--nth"] = false }
-  --   opts.no_esc = true
-  --   opts.search = opts.search or ""
-  --   fzf_lua.grep_project(opts)
-  -- end
-  --
-  -- vim.keymap.set("n", "<leader>/", grep_project, { desc = "grep_project" })
-  -- vim.keymap.set("n", "<c-s>", function()
-  --   grep_project({ prompt = "Notes> ", cwd = require("vars").notes_dir })
-  -- end, { desc = "grep_notes" })
 end
 
 return M

72a5480f1a344a1ab2408e69f9bc1ff9bc34a3ed
illiadanko <illia.a.danko@gmail.com>
2023-06-13 22:17:37 +0200

    Add guard check

--- lua/lang/my_orgmode.lua
+++ lua/lang/my_orgmode.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 M.config = function()
-  local orgmode = require("orgmode")
+  local ok, orgmode = pcall(require, "orgmode")
+  if not ok then
+    return
+  end
+
   orgmode.setup_ts_grammar()
   orgmode.setup({})
 end

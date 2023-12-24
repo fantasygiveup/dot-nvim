fbfb877bbe6f092186abcf68f515daa969a57e86
illiadanko <illia.a.danko@gmail.com>
2023-12-24 14:03:25 +0200

    Neorg: add basic mapping

--- lua/tools/neorg.lua
+++ lua/tools/neorg.lua
@@ -13,9 +13,11 @@ M.config = function()
 
   neorg.setup({
     load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.dirman"] = { -- Manages Neorg workspaces
+      ["core.defaults"] = {}, -- loads default behaviour
+      ["core.concealer"] = {}, -- adds pretty icons to your documents
+      ["core.pivot"] = {}, -- toggle list types (ordered to unordered and vise versa)
+      ["core.itero"] = {}, -- repeat same operation
+      ["core.dirman"] = { -- manages Neorg workspaces
         config = {
           workspaces = {
             notes = vars.org_dir .. vars.path_sep .. "notes",
@@ -24,17 +26,60 @@ M.config = function()
           default_workspace = "notes",
         },
       },
-      ["core.keybinds"] = {
-        config = {
-          neorg_leader = "<leader>",
-        },
-      },
+      ["core.keybinds"] = { config = { neorg_leader = "<leader>" } },
     },
   })
 
   vim.keymap.set("n", "<leader>nw", ":Neorg workspace ", { desc = "neorg workspace" })
   vim.keymap.set("n", "<leader>ni", "<cmd>Neorg index<cr>", { desc = "neorg index" })
-  vim.keymap.set("n", "<leader>nr", "<cmd>Neorg return<cr>", { desc = "neorg return" })
+
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = vim.api.nvim_create_augroup("NorgGroup", {}),
+    pattern = { "*.norg" },
+    callback = function()
+      vim.keymap.set(
+        "n",
+        "<leader>n<cr>",
+        "<cmd>Neorg return<cr>",
+        { desc = "neorg return", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>n<Space>",
+        "<cmd>Neorg keybind norg core.qol.todo_items.todo.task_cycle<CR>",
+        { desc = "neorg cycle task", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>nc",
+        "<cmd>Neorg toggle-concealer<cr>",
+        { desc = "neorg toggle concealer", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>nl",
+        "<cmd>Neorg keybind norg core.pivot.toggle-list-type<cr>",
+        { desc = "neorg toggle list type", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "i",
+        "<c-cr>",
+        "<cmd>Neorg keybind all core.itero.next-iteration<cr>",
+        { desc = "neorg repeat last", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>ni",
+        "<cmd>Neorg inject-metadata<cr>",
+        { desc = "neorg inject metadata", buffer = 0 }
+      )
+    end,
+  })
 end
 
 return M

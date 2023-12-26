d9db67cde3acd57a091a0cd28a8c323185f30487
illiadanko <illia.a.danko@gmail.com>
2023-12-26 22:20:32 +0200

    Fix gitlinker keymap

--- lua/vc/my_gitlinker.lua
+++ lua/vc/my_gitlinker.lua
@@ -17,15 +17,19 @@ M.config = function()
 
   vim.keymap.set("n", "<localleader>gu", function()
     gitlinker.get_buf_range_url("n", { action_callback = actions.copy_to_clipboard })
-  end, { desc = "vc_url_at_point" })
+  end, { desc = "copy git line to clipboard" })
 
   vim.keymap.set("v", "<localleader>gu", function()
     gitlinker.get_buf_range_url("v", { action_callback = actions.copy_to_clipboard })
-  end, { desc = "vc_url_range" })
+  end, { desc = "copy git range to clipboard" })
 
   vim.keymap.set("n", "<localleader>gU", function()
     gitlinker.get_repo_url({ action_callback = actions.open_in_browser })
-  end, { desc = "vc_open_in_browser" })
+  end, { desc = "open git line in browser" })
+
+  vim.keymap.set("v", "<localleader>gU", function()
+    gitlinker.get_buf_range_url("v", { action_callback = actions.open_in_browser })
+  end, { desc = "open selected git range in browser" })
 end
 
 return M

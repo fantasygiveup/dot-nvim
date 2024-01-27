2df50f22aa2c48413ddee345a0c3c87be3f06916
illiadanko <illia.a.danko@gmail.com>
2024-01-27 02:14:11 +0200

    Use neorg over org

--- after/ftplugin/org.lua
+++
@@ -1,4 +0,0 @@
-vim.wo.conceallevel = 2
-vim.wo.concealcursor = "nc"
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -103,4 +103,71 @@ M.lsp_flow = function(bufnr)
   vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 end
 
+M.neorg = function()
+  vars = require("vars")
+
+  vim.keymap.set("n", "<leader>nw", ":Neorg workspace ", { desc = "neorg workspace" })
+  vim.keymap.set("n", "<leader>ni", "<cmd>Neorg index<cr>", { desc = "neorg index" })
+  vim.keymap.set(
+    "n",
+    "<leader>nt",
+    "<cmd>e " .. vars.org_dir .. vars.path_sep .. "todos.norg" .. " <cr>",
+    { desc = "open todo file" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>ns",
+    "<cmd>e " .. vars.org_dir .. vars.path_sep .. "scratchpad.norg" .. " <cr>",
+    { desc = "open scratchpad file" }
+  )
+
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = vim.api.nvim_create_augroup("NorgGroup", {}),
+    pattern = { "*.norg" },
+    callback = function()
+      vim.keymap.set(
+        "n",
+        "<leader><cr>",
+        "<cmd>Neorg return<cr>",
+        { desc = "neorg return", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>n<Space>",
+        "<cmd>Neorg keybind norg core.qol.todo_items.todo.task_cycle<CR>",
+        { desc = "neorg cycle task", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>nc",
+        "<cmd>Neorg toggle-concealer<cr>",
+        { desc = "neorg toggle concealer", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>nl",
+        "<cmd>Neorg keybind norg core.pivot.toggle-list-type<cr>",
+        { desc = "neorg toggle list type", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        { "i", "n" },
+        "<c-cr>",
+        "<cmd>Neorg keybind all core.itero.next-iteration<cr>",
+        { desc = "neorg repeat last", buffer = 0 }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<leader>nh",
+        "<cmd>Neorg inject-metadata<cr>",
+        { desc = "neorg insert header", buffer = 0 }
+      )
+    end,
+  })
+end
+
 return M

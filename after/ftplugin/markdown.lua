3535346ab775caf0637d3d45aa51469b8a0c0628
illiadanko <illia.a.danko@gmail.com>
2024-02-01 21:37:47 +0100

    Update markdown conceal and folding

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
+vim.wo.concealcursor = "nc" -- reveal on insert
 vim.wo.conceallevel = 2
 vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"

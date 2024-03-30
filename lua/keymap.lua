e01ef31696a552cb189f279425dc1370021b14f2
illiadanko <illia.a.danko@gmail.com>
2024-03-30 17:54:19 +0100

    Update keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -271,7 +271,7 @@ M.cmp_preset = function()
     ["<CR>"] = cmp.mapping({
       i = function(fallback)
         if cmp.visible() and cmp.get_active_entry() then
-          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          cmp.confirm({ select = false })
         else
           fallback()
         end

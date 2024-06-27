6920453a0d4b124b4366cc862e85fe29d171c2cb
illiadanko <illia.a.danko@gmail.com>
2024-06-27 10:32:45 +0300

    Use settings outside neovim config dir

--- lua/custom.lua
+++ lua/custom.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.config = function()
+M.init = function()
   local custom_settings_file = vim.fn.expand("$HOME/.custom_nvim_settings.lua")
   if require("utils.file").file_exists(custom_settings_file) then
     dofile(custom_settings_file)

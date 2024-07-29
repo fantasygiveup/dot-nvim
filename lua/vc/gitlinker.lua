c5f8c660b9dbf3bda354ba2be5a675cbf9f2294a
illiadanko <illia.a.danko@gmail.com>
2024-07-29 19:38:31 +0300

    Update gitlinker

--- lua/vc/gitlinker.lua
+++ lua/vc/gitlinker.lua
@@ -12,7 +12,38 @@ M.config = function()
   end
 
   gitlinker.setup({
-    mappings = nil, -- don't use default mappings
+    router = {
+      browse = {
+        ["^bitbucket%.dentsplysirona%.com"] = "https://bitbucket.dentsplysirona.com/"
+          .. "projects/"
+          .. "{_A.ORG}/repos/"
+          .. "{_A.REPO}/browse/"
+          .. "{_A.FILE}"
+          .. "?at=refs/heads/{_A.CURRENT_BRANCH}"
+          .. "#{_A.LSTART}"
+          .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+      },
+      default_branch = {
+        ["^bitbucket%.dentsplysirona%.com"] = "https://bitbucket.dentsplysirona.com/"
+          .. "projects/"
+          .. "{_A.ORG}/repos/"
+          .. "{_A.REPO}/browse/"
+          .. "{_A.FILE}"
+          .. "?at=refs/heads/{_A.DEFAULT_BRANCH}"
+          .. "#{_A.LSTART}"
+          .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+      },
+      current_branch = {
+        ["^bitbucket%.dentsplysirona%.com"] = "https://bitbucket.dentsplysirona.com/"
+          .. "projects/"
+          .. "{_A.ORG}/repos/"
+          .. "{_A.REPO}/browse/"
+          .. "{_A.FILE}"
+          .. "?at=refs/heads/{_A.CURRENT_BRANCH}"
+          .. "#{_A.LSTART}"
+          .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+      },
+    },
   })
 
   require("keymap").git_linker()

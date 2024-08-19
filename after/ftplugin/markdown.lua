3f8ddf091a6ea4259626cdd509c121aa67d8c9f6
illiadanko <illia.a.danko@gmail.com>
2024-08-19 08:00:13 +0300

    Disable break lines for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,4 +5,8 @@ vim.opt_local.foldnestmax = 3
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt_local.textwidth = 80
+vim.opt_local.wrap = false
+-- 1. Do not auto break lines.
+-- 2. Turn off autowrap links.
+vim.opt_local.formatoptions = "jqln"
 -- vim.api.nvim_set_hl(0, "@codeblock", { link = "Visual" }) -- custom markdown tree-sitter highlight

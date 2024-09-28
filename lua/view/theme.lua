8419beabf95db924a5d2b5ae5970e3f04775edf7
illiadanko <illia.a.danko@gmail.com>
2024-09-28 21:24:14 +0300

    Use catppuccin theme over onedark

--- lua/view/theme.lua
+++ lua/view/theme.lua
@@ -1,43 +1,13 @@
 local M = {}
 
 M.config = function()
-  local ok, theme = pcall(require, "onedark")
-  if not ok then
-    return
-  end
-
   M.load_background()
 
-  theme.setup({
-    style = vim.o.background,
-    highlights = {
-      QuickFixLine = { fmt = "none" },
-      -- Override https://github.com/rcarriga/nvim-notify colors.
-      NotifyINFOTitle = { fg = "$green" },
-      NotifyINFOIcon = { fg = "$green" },
-      NotifyINFOBorder = { fg = "$green" },
-      -- Markdown | Zettelkasten.
-      ["@markup.link.label"] = { fg = "$blue", fmt = "bold" },
-      ["@markup.link"] = { fg = "$grey", fmt = "none" },
-      markdownAutomaticLink = { fg = "$cyan", fmt = "bold" },
-      ["@markup.link.url"] = { fg = "none", fmt = "none" },
-      markdownUrl = { fmt = "none" },
-      markdownLink = { fmt = "none" },
-    }, -- overrides
-  })
-  theme.load()
-end
-
-M.listen_to_system_background_change = function()
-  local ok, fwatch = pcall(require, "fwatch")
-  if not ok then
-    return
-  end
-
-  fwatch.watch(require("vars").system_theme_file, {
-    on_event = function()
-      M.load_background_async()
-    end,
+  require("catppuccin").setup({
+    background = { -- :h background
+      light = "latte",
+      dark = "mocha",
+    },
   })
 end
 
@@ -53,14 +23,8 @@ M.load_background = function()
   vim.o.background = M.system_theme(system_theme_file)
 end
 
-M.load_background_async = function()
-  require("utils.async").timer(function()
-    require("view.theme").load_background()
-  end)
-end
-
 M.init = function()
-  M.listen_to_system_background_change()
+  vim.cmd.colorscheme("catppuccin")
 end
 
 return M

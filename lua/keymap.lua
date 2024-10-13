9f001e4b9543f1a2f91c65b20beaacb807c76f6f
illiadanko <illia.a.danko@gmail.com>
2024-10-13 07:22:41 +0300

    Update smart split keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -443,15 +443,15 @@ M.git_linker = function()
 end
 
 M.terminal_multiplexer = function()
-  vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-  vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-  vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-  vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+  vim.keymap.set("n", "<c-h>", require("smart-splits").move_cursor_left)
+  vim.keymap.set("n", "<c-j>", require("smart-splits").move_cursor_down)
+  vim.keymap.set("n", "<c-k>", require("smart-splits").move_cursor_up)
+  vim.keymap.set("n", "<c-l>", require("smart-splits").move_cursor_right)
 
-  vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-  vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-  vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-  vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+  vim.keymap.set("n", "<a-s-h>", require("smart-splits").resize_left)
+  vim.keymap.set("n", "<a-s-j>", require("smart-splits").resize_down)
+  vim.keymap.set("n", "<a-s-k>", require("smart-splits").resize_up)
+  vim.keymap.set("n", "<a-s-l>", require("smart-splits").resize_right)
 end
 
 M.gitsigns = function()

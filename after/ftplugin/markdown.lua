b8d50c424924394e1188ea12e2f9c71f7f696cea
illiadanko <illia.a.danko@gmail.com>
2025-01-27 12:02:46 +0200

    Decrease markdown fold level

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 vim.opt_local.concealcursor = "nc" -- reveal on insert
 vim.opt_local.conceallevel = 0 -- disable conceal by default
 vim.opt_local.foldlevel = 0 -- close all folds by default
-vim.opt_local.foldnestmax = 3
+vim.opt_local.foldnestmax = 1
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt_local.textwidth = 80

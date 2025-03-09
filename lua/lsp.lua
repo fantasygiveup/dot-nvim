e50b28100d1618d3f604f297a1675271896c27c4
fantasygiveup <illia@danko.me>
2025-03-09 16:12:35 +0100

    Add dart language server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -157,6 +157,26 @@ M.config = function()
       },
     },
   })
+
+  lspconfig.dartls.setup({
+    cmd = { "dart", "language-server", "--protocol=lsp" },
+    filetypes = { "dart" },
+    init_options = {
+      closingLabels = true,
+      flutterOutline = true,
+      onlyAnalyzeProjectsWithOpenFiles = true,
+      outline = true,
+      suggestFromUnimportedLibraries = true,
+    },
+    -- root_dir = root_pattern("pubspec.yaml"),
+    settings = {
+      dart = {
+        completeFunctionCalls = true,
+        showTodos = true,
+      },
+    },
+    on_attach = on_attach,
+  })
 end
 
 return M

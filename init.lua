fantasygiveup/dot-nvim

commit b308dcbbd50a00e92161fc2525879cc745c08315

Watch system color theme for a change

init.lua
@@ -35,6 +35,7 @@ lazy.setup({
     name = "catppuccin",
     priority = 1000,
     config = require("view.theme").config,
+    dependencies = { "rktjmp/fwatch.nvim" },
   },
   { "folke/which-key.nvim", config = require("view.which_key").config },
   { "nvim-lualine/lualine.nvim" },
@@ -120,7 +121,7 @@ lazy.setup({
 })
 
 -- NOTE: "color.lua" with a "background" option is generated by an external tool.
-pcall(require, "color")
+pcall(require, "color/color")
 
 require("event").init()
 require("tools.gpg").init()

lua/view/theme.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 M.config = function()
-  M.load_background()
-
   require("catppuccin").setup({
     background = { -- :h background
       light = "latte",
@@ -32,25 +30,32 @@ M.config = function()
   })
 end
 
-M.system_theme = function(theme_file)
-  local fd = io.open(theme_file)
-  -- Fallback to the light theme.
+M.listen_to_system_theme_change = function()
+  local fwatch = require("fwatch")
+  local vars = require("vars")
+  local dir = vim.fn.stdpath("config") .. vars.path_sep .. "lua" .. vars.path_sep .. "color"
+  local file = dir .. vars.path_sep .. "color.lua"
+
+  local fd = io.open(file)
   if not fd then
-    return "light"
+    error("unable to monitor color.lua")
+    return
   end
+  fd.close()
 
-  local theme = fd:read()
-  fd:close()
-  return theme
-end
-
-M.load_background = function()
-  local system_theme_file = require("vars").system_theme_file
-  vim.o.background = M.system_theme(system_theme_file)
+  fwatch.watch(dir, {
+    on_event = function()
+      require("utils.async").timer(function()
+        dofile(file)
+        require("status_line").config() -- redraw status_line entirely
+      end)
+    end,
+  })
 end
 
 M.init = function()
   vim.cmd.colorscheme("catppuccin")
+  M.listen_to_system_theme_change()
 end
 
 return M

.gitignore
@@ -1,1 +1,2 @@
 lazy-lock.json
+lua/color

lua/vars.lua
@@ -33,13 +33,6 @@ function vars:load_variables()
   self.diagnostic_severity = vim.diagnostic.severity.ERROR
   self.rg_opts = "--column --line-number --no-heading --color=always --colors='match:none' --smart-case --max-columns=512 --hidden "
     .. (vim.env.RIPGREP_IGNORE_SEARCH_FILTER or "") -- ignore the missing value in restricted environments
-  self.system_theme_file = home
-    .. path_sep
-    .. ".config"
-    .. path_sep
-    .. "appearance"
-    .. path_sep
-    .. "background"
   self.fleeting_notes = notes_dir_path .. path_sep .. "sm9z.md"
 end
 

commit 2feee3a5fc8af34b3878f574720d4506ea286413

Fix issue introduced by commit b308dcbbd50a

init.lua
@@ -120,13 +120,9 @@ lazy.setup({
   },
 })
 
--- NOTE: "color.lua" with a "background" option is generated by an external tool.
-pcall(require, "color/color")
-
 require("event").init()
 require("tools.gpg").init()
 require("fzf.project").init()
-require("view.theme").init()
-require("status_line").config() -- draw the status line after the theme gets initialized
 require("custom").init()
 require("treesitter.markdown").init()
+require("view.theme").init()

lua/status_line.lua
@@ -1,5 +1,19 @@
 local M = {}
 
+local function color_hl2rgb(name, background)
+  local background = background or false
+  local color_val = vim.api.nvim_get_hl_by_name(name, true)
+  if color_val == -1 then
+    return "#" .. name
+  end
+
+  if background then
+    return string.format("#%06x", color_val.background)
+  else
+    return string.format("#%06x", color_val.foreground)
+  end
+end
+
 M.config = function()
   local lualine = require("lualine")
 
@@ -50,9 +64,6 @@ M.config = function()
     end
   end
 
-  local flavour = require("catppuccin").flavour
-  local clrs = require("catppuccin.palettes").get_palette(flavour)
-
   lualine.setup({
     options = {
       theme = "catppuccin",
@@ -66,7 +77,10 @@ M.config = function()
         "%=",
         {
           spelling,
-          color = { bg = clrs.surface0, fg = clrs.red },
+          color = {
+            bg = color_hl2rgb("ColorColumn", true),
+            fg = color_hl2rgb("ErrorMsg"),
+          },
           separator = { right = "", left = "" },
         },
       },
@@ -75,14 +89,14 @@ M.config = function()
           lsp_active_clients,
           separator = { right = "", left = "" },
           right_padding = 2,
-          color = { bg = clrs.surface0, fg = clrs.yellow },
+          color = { bg = color_hl2rgb("ColorColumn", true), fg = color_hl2rgb("WarningMsg") },
         },
         {
           "diagnostics",
           sources = { "nvim_diagnostic" },
           sections = diagnostics_severity(),
           symbols = { error = " ", warn = " ", info = " ", hint = " " },
-          color = { bg = clrs.surface0, fg = clrs.red },
+          color = { bg = color_hl2rgb("ColorColumn", true), fg = color_hl2rgb("ErrorMsg") },
           separator = { right = "", left = "" },
         },
         "encoding",

lua/view/theme.lua
@@ -53,7 +53,16 @@ M.listen_to_system_theme_change = function()
 end
 
 M.init = function()
+  -- NOTE: "color.lua" with a "background" option is generated by an external tool.
+  local ok = pcall(require, "color/color")
+  if not ok then
+    vim.o.background = "light" -- fallback to the light variant.
+  end
+
   vim.cmd.colorscheme("catppuccin")
+
+  require("status_line").config()
+
   M.listen_to_system_theme_change()
 end
 